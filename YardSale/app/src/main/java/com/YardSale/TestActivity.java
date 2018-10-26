5084202b94711a7b4cf41435ffc9056c3364b1cd
nadeneAmara <nadeneabuamara@gmail.com>
2018-10-25 18:05:29 -0700

    added search interface

--- YardSale/app/src/main/java/com/YardSale/TestActivity.java
+++ YardSale/app/src/main/java/com/YardSale/TestActivity.java
@@ -10,4 +10,18 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
 }
