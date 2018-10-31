f066ae87f1e1793f0847f73dd723b02af13a56fd
sean.bell <sean.bell@servicenow.com>
2018-10-30 20:21:47 -0700

    added login page, not done

--- YardSale/app/src/main/java/com/YardSale/CreateNewUser.java
+++ YardSale/app/src/main/java/com/YardSale/CreateNewUser.java
@@ -19,7 +19,7 @@ public class CreateNewUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_user)
+        //setContentView(R.layout.activity_create_new_user)
 
     }
 
