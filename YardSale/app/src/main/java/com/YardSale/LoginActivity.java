7149a4e39c1cce1a550cedf84ae2e71998160e29
sean.bell <sean.bell@servicenow.com>
2018-11-07 14:01:32 -0800

    Fixed most of the merge issues

--- YardSale/app/src/main/java/com/YardSale/LoginActivity.java
+++ YardSale/app/src/main/java/com/YardSale/LoginActivity.java
@@ -1,11 +1,8 @@
-package com.YardSale.Activities;
+package com.YardSale;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.database.Cursor;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,8 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.YardSale.MainMenuActivity;
-import com.YardSale.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -102,7 +97,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Log.d(TAG, "createUserWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 Intent myIntent = new Intent(LoginActivity.this,
-                                        MainMenuActivity.class);
+                                        MainSearchActivity.class);
                                 startActivity(myIntent);
                             } else {
                                 // If sign in fails, display a message to the user.
@@ -138,7 +133,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Log.d(TAG, "loginUserWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 Intent myIntent = new Intent(LoginActivity.this,
-                                        MainMenuActivity.class);
+                                        MainSearchActivity.class);
                                 startActivity(myIntent);
                             } else {
                                 // If sign in fails, display a message to the user.
