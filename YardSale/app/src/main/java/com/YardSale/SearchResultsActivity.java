c341b01b1893da8282a79edfd8c443c5df42d6ad
nadeneAmara <nadeneabuamara@gmail.com>
2018-11-18 13:26:01 -0800

    searches zipcode values (builds)

--- YardSale/app/src/main/java/com/YardSale/SearchResultsActivity.java
+++ YardSale/app/src/main/java/com/YardSale/SearchResultsActivity.java
@@ -4,8 +4,27 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.YardSale.models.Post;
+import com.YardSale.models.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+
 
 public class SearchResultsActivity extends Activity {
+    TextView tSResult = (TextView) findViewById(R.id.text_view_id);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,31 +40,31 @@ public class SearchResultsActivity extends Activity {
     }
 
     private void handleIntent(Intent intent) {
+
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
+            DatabaseReference mDatabase;
             //use the query to search your data somehow
-        }
-    }
+            mDatabase = FirebaseDatabase.getInstance().getReference("posts");
+            Query query2 = mDatabase.orderByChild("zipcode").equalTo(query);
+            query2.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.getChildrenCount() > 0){
+                        System.out.println("count : "+ dataSnapshot.getChildrenCount());
+                        for(DataSnapshot child : dataSnapshot.getChildren()){
+                            String value = child.getValue(String.class);
+                            tSResult.setText(value);
+                        }
+                    }
 
-    mDatabase = FirebaseDatabase.getInstance().getReference("user-posts");
-    Query  query = mDatabase.orderByChild("zipcode").equalTo(zipcode);
-            query .addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            if(dataSnapshot.getChildrenCount() > 0){
-                System.out.println("count : "+ dataSnapshot.getChildrenCount());
-                for(DataSnapshot child : dataSnapshot.getChildren()){
-                    value = child.getValue(String.class);
-                    tSResult.setText(value);
                 }
-            }
 
-        }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
-        }
-    });
-
+                }
+        });
+     }
+    }
 }
