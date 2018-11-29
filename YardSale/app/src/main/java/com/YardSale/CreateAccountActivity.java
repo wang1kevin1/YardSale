890c40b90dfedb7be4fa9c4522c735917fe301d9
Yinan Mao <ymao7@ucsc.edu>
2018-11-28 23:24:39 -0800

    layout design
    redesign the layout of login and create account

--- YardSale/app/src/main/java/com/YardSale/CreateAccountActivity.java
+++ YardSale/app/src/main/java/com/YardSale/CreateAccountActivity.java
@@ -1,11 +1,14 @@
 package com.YardSale;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -107,7 +110,7 @@ public class CreateAccountActivity extends AppCompatActivity {
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 AccountUtil.writeUser(user.getUid(), user.getEmail(), zipcode);
                                 Intent myIntent = new Intent(CreateAccountActivity.this,
-                                        MainSearchActivity.class);
+                                        Navigation.class);
                                 startActivity(myIntent);
                             } else {
                                 // If sign in fails, display a message to the user.
@@ -119,5 +122,41 @@ public class CreateAccountActivity extends AppCompatActivity {
                     });
         }
     }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+    public  boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = { 0, 0 };
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 
