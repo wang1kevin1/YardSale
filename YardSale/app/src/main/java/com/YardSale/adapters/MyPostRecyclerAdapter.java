1cb42048a4137fc180febf2b2bff556e73121484
Kevin Wang <wang1kevin1@gmail.com>
2018-12-02 18:48:09 -0800

    Added contact seller button with seller email and default subject + message

--- YardSale/app/src/main/java/com/YardSale/adapters/MyPostRecyclerAdapter.java
+++ YardSale/app/src/main/java/com/YardSale/adapters/MyPostRecyclerAdapter.java
@@ -13,9 +13,13 @@ import android.widget.TextView;
 
 import com.YardSale.R;
 import com.YardSale.models.Post;
+import com.YardSale.models.User;
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -71,13 +75,27 @@ public class MyPostRecyclerAdapter extends RecyclerView.Adapter<MyPostRecyclerAd
         holder.vContactSeller.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                Uri data = Uri.parse("mailto:" + "sellerEmail" +
-                        "?subject=" + "Interested in your YardSale Post" +
-                        "&body=" + "I'm interested in your listing for: " +
-                        title + ".");
-                intent.setData(data);
-                context.startActivity(intent);
+                //email stuff
+                mDatabase.child("users").child(uid).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        String sellerEmail = user.getEMAIL();
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        Uri data = Uri.parse("mailto:" + sellerEmail +
+                                "?subject=" + "Interested in your YardSale Post" +
+                                "&body=" + "I'm interested in your listing for: \"" +
+                                title + "\".");
+                        intent.setData(data);
+                        context.startActivity(intent);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        System.out.println("The read failed: " + databaseError.getCode());
+                    }
+                });
             }
         });
     }
